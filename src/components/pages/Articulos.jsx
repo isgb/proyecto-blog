7185a4632253756f9a2a8acce496917cdaf55a18
isaias_pruebas <isgb_codigo@hotmail.com>
2024-01-23 23:14:36 -0600

    debug

--- src/components/pages/Articulos.jsx
+++ src/components/pages/Articulos.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React from "react";
 
 export const Articulos = () => {
   return (
-    <div>Articulos</div>
-  )
-}
+    <>
+      <article className="articulo-item">
+        <div className="mask">
+
+        </div>
+        <h3 className="title">Desarrollo web</h3>
+        <p className="description">victorroblesweb.es</p>
+
+        <button className="edit">Editar</button>
+        <button className="delete">Borrar</button>
+      </article>
+
+      <article className="articulo-item">
+        <h3 className="title">Desarrollo web</h3>
+        <p className="description">victorroblesweb.es</p>
+
+        <button className="edit">Editar</button>
+        <button className="delete">Borrar</button>
+      </article>
+
+      <article className="articulo-item">
+        <h3 className="title">Desarrollo web</h3>
+        <p className="description">victorroblesweb.es</p>
+
+        <button className="edit">Editar</button>
+        <button className="delete">Borrar</button>
+      </article>
+
+      <article className="articulo-item">
+        <h3 className="title">Desarrollo web</h3>
+        <p className="description">victorroblesweb.es</p>
+
+        <button className="edit">Editar</button>
+        <button className="delete">Borrar</button>
+      </article>
+    </>
+  );
+};
