efe47f909cd4a76e10a248ef9544678bb6ace92f
isaias_pruebas <isgb_codigo@hotmail.com>
2024-01-23 23:09:18 -0600

    debug

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Inicio = () => {
   return (
-    <div>Inicio</div>
+    <div className='jumbo'>
+      <h1>Bienvenido al blog con React</h1>
+      <p>Bolg desarrollado con el MERN Stack (Mongo, Express, React, Nodejs)</p>
+      <Link to="/articulos" className='button'>Ver los articulos</Link>
+    </div>
   )
 }
