34425b61ccddec173eb2d90c87659f515aff76d2
isaias_pruebas <isgb_codigo@hotmail.com>
2024-01-23 22:02:46 -0600

    debug

---
+++ arrancar-proyecto copy.bat
@@ -0,0 +1,5 @@
+start cmd.exe /k "cd C:\Program Files\MongoDB\Server\6.0\bin && mongod.exe"
+start cmd.exe /k "cd C:\Users\isgb_\Documents\cursos udemy\VICTOR ROBLES\api-rest-node-main && npm start"
+start cmd.exe /k "cd C:\Users\isgb_\Documents\cursos udemy\VICTOR ROBLES\proyecto-blog && npm run dev"
+start C:\Users\isgb_\AppData\Local\MongoDBCompass\MongoDBCompass.exe
+start C:\Users\Zentro2\AppData\Local\insomnia\Insomnia.exe
