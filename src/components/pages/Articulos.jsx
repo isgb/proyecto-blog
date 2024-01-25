43707b25aa5d6c77b8ba37f6547a8308731d83e5
isaias_pruebas <isgb_codigo@hotmail.com>
2024-01-24 23:55:39 -0600

    debug

--- src/components/pages/Articulos.jsx
+++ src/components/pages/Articulos.jsx
@@ -31,22 +31,29 @@ export const Articulos = () => {
   return (
     <>
     {
-      articulos.map(articulo => {
-        return (
-        <article key={articulo._id} className="articulo-item">
-          <div className="mascara">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png" />
-          </div>
-          <div className="datos">
-            <h3 className="title">{articulo.titulo}</h3>
-            <p className="description">{articulo.contenido}</p>
-
-            <button className="edit">Editar</button>
-            <button className="delete">Borrar</button>
-          </div>
-        </article>
-        )
-      })
+      articulos.length >= 1 ? (
+        articulos.map(articulo => {
+          return (
+          <article key={articulo._id} className="articulo-item">
+            <div className="mascara">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png" />
+            </div>
+            <div className="datos">
+              <h3 className="title">{articulo.titulo}</h3>
+              <p className="description">{articulo.contenido}</p>
+  
+              <button className="edit">Editar</button>
+              <button className="delete">Borrar</button>
+            </div>
+          </article>
+          )
+        })
+      ) 
+      
+      :
+      (
+        <h1>No hay artículos</h1>
+      )
     }
       
     </>
