66ac67cb49c1764101005e3765316a878769ffd8
oscar <oscar.vasquez@zentro.com.mx>
2024-01-25 16:00:25 -0600

    debug

--- src/components/pages/Crear.jsx
+++ src/components/pages/Crear.jsx
@@ -2,6 +2,29 @@ import React from 'react'
 
 export const Crear = () => {
   return (
-    <div>Crear</div>
+    <div className='jumbo'>
+      <h1>Crear artículo</h1>
+      <p>Formulario para crear un artículo</p>
+
+      <form action="">
+        <div className='form-group'>
+          <label htmlFor='titulo'>Titulo</label>
+          <input type="text" name='titulo' />
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='contenido'>Contenido</label>
+          <textarea type="text" name='contenido' />
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='file0'>Imagen</label>
+          <input type="file" name='file0' id='file'/>
+        </div>
+
+        <input type="submit" value='Guardar' className='btn btn-success'/>
+      </form>
+
+    </div>
   )
 }
