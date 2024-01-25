d69b69e6038c8617d6ef341ded755bdd07982907
oscar <oscar.vasquez@zentro.com.mx>
2024-01-25 15:30:03 -0600

    debug

--- src/components/pages/Articulos.jsx
+++ src/components/pages/Articulos.jsx
@@ -5,6 +5,7 @@ import { Peticion } from "../../helpers/Peticion";
 export const Articulos = () => {
 
   const [articulos, setArticulos] = useState([]);
+  const [cargando, setCargando] = useState(true);
 
   const conseguirArticulos = async () => {
 
@@ -23,6 +24,7 @@ export const Articulos = () => {
       setArticulos(datos.articulos)
     }
 
+    setCargando(false)
   }
 
   useEffect(() => {
@@ -33,31 +35,35 @@ export const Articulos = () => {
 
   return (
     <>
-      {
-        articulos.length >= 1 ? (
-          articulos.map(articulo => {
-            return (
-              <article key={articulo._id} className="articulo-item">
-                <div className="mascara">
-                  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png" />
-                </div>
-                <div className="datos">
-                  <h3 className="title">{articulo.titulo}</h3>
-                  <p className="description">{articulo.contenido}</p>
+      {cargando ? "Cargando..." :
+        (
 
-                  <button className="edit">Editar</button>
-                  <button className="delete">Borrar</button>
-                </div>
-              </article>
-            )
-          })
-        )
+          articulos.length >= 1 ? (
+            articulos.map(articulo => {
+              return (
+                <article key={articulo._id} className="articulo-item">
+                  <div className="mascara">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png" />
+                  </div>
+                  <div className="datos">
+                    <h3 className="title">{articulo.titulo}</h3>
+                    <p className="description">{articulo.contenido}</p>
 
-          :
-          (
-            <h1>No hay artículos</h1>
+                    <button className="edit">Editar</button>
+                    <button className="delete">Borrar</button>
+                  </div>
+                </article>
+              )
+            })
           )
-      }
+
+            :
+            (
+              <h1>No hay artículos</h1>
+            )
+
+        )}
+
 
     </>
   );
