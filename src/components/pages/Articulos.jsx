40d84c0885b22a7217f7477c777cf5e3dc27b1e7
isaias_pruebas <isgb_codigo@hotmail.com>
2024-01-24 22:57:28 -0600

    debug

--- src/components/pages/Articulos.jsx
+++ src/components/pages/Articulos.jsx
@@ -4,38 +4,16 @@ export const Articulos = () => {
   return (
     <>
       <article className="articulo-item">
-        <div className="mask">
-
+        <div className="mascara">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png" />
         </div>
-        <h3 className="title">Desarrollo web</h3>
-        <p className="description">victorroblesweb.es</p>
+        <div className="datos">
+          <h3 className="title">Desarrollo web</h3>
+          <p className="description">victorroblesweb.es</p>
 
-        <button className="edit">Editar</button>
-        <button className="delete">Borrar</button>
-      </article>
-
-      <article className="articulo-item">
-        <h3 className="title">Desarrollo web</h3>
-        <p className="description">victorroblesweb.es</p>
-
-        <button className="edit">Editar</button>
-        <button className="delete">Borrar</button>
-      </article>
-
-      <article className="articulo-item">
-        <h3 className="title">Desarrollo web</h3>
-        <p className="description">victorroblesweb.es</p>
-
-        <button className="edit">Editar</button>
-        <button className="delete">Borrar</button>
-      </article>
-
-      <article className="articulo-item">
-        <h3 className="title">Desarrollo web</h3>
-        <p className="description">victorroblesweb.es</p>
-
-        <button className="edit">Editar</button>
-        <button className="delete">Borrar</button>
+          <button className="edit">Editar</button>
+          <button className="delete">Borrar</button>
+        </div>
       </article>
     </>
   );
