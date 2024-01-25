c606778538349c93fe7a10d1317fce2af92e252f
isaias_pruebas <isgb_codigo@hotmail.com>
2024-01-24 23:35:13 -0600

    debug

--- src/components/pages/Articulos.jsx
+++ src/components/pages/Articulos.jsx
@@ -5,42 +5,45 @@ export const Articulos = () => {
   const [articulos, setArticulos] = useState([]);
 
   useEffect(() => {
-    
-    let data = [
-      {
-        _id:1,
-        titulo: "Titulo 1",
-        contenido : "Contenido"
-      },
-      {
-        _id:2,
-        titulo: "Titulo 1",
-        contenido : "Contenido"
-      },
-      {
-        _id:3,
-        titulo: "Titulo 1",
-        contenido : "Contenido"
-      },
-    ];
+
+   conseguirArticulos()
 
   }, [])
   
+  const conseguirArticulos = async () =>{
+
+    const url = "http://localhost:3900/api/articulos";
+    let peticion = await fetch(url, {
+      method: "GET"
+    });
+
+    let datos = await peticion.json()
+
+    console.log("datos:",datos)
+
+  }
 
   return (
     <>
-      <article className="articulo-item">
-        <div className="mascara">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png" />
-        </div>
-        <div className="datos">
-          <h3 className="title">Desarrollo web</h3>
-          <p className="description">victorroblesweb.es</p>
+    {
+      articulos.map(articulo => {
+        return (
+        <article key={articulo._id} className="articulo-item">
+          <div className="mascara">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png" />
+          </div>
+          <div className="datos">
+            <h3 className="title">{articulo.titulo}</h3>
+            <p className="description">{articulo.contenido}</p>
 
-          <button className="edit">Editar</button>
-          <button className="delete">Borrar</button>
-        </div>
-      </article>
+            <button className="edit">Editar</button>
+            <button className="delete">Borrar</button>
+          </div>
+        </article>
+        )
+      })
+    }
+      
     </>
   );
 };
