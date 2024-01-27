c149815730e22433dd7f8ff35d729ef141acd44d
isaias_pruebas <isgb_codigo@hotmail.com>
2024-01-27 13:39:26 -0600

    debug

--- src/components/pages/Busqueda.jsx
+++ src/components/pages/Busqueda.jsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Global } from "../../helpers/Global";
+import { Peticion } from "../../helpers/Peticion";
+import { Listado } from "./Listado";
+import { useParams } from "react-router-dom";
 
 export const Busqueda = () => {
+
+  const [articulos, setArticulos] = useState([]);
+  const [cargando, setCargando] = useState(true);
+  const params = useParams();
+
+  const conseguirArticulos = async () => {
+
+    // const url = Global.url+"articulos";
+    const { datos, cargando } = await Peticion(Global.url + "buscar/"+params.busqueda, "GET")
+    /*
+        let peticion = await fetch(url, {
+          method: "GET"
+        });
+    
+        let datos = await peticion.json() 
+    */
+
+    console.log("datos:", datos)
+    if (datos.status === "success") {
+      setArticulos(datos.articulos)
+    }
+
+    setCargando(false)
+  }
+
+  useEffect(() => {
+    conseguirArticulos()
+  }, [])
+
+  useEffect(() => {
+    conseguirArticulos()
+  }, [params])
+
   return (
-    <div>Busqueda</div>
-  )
-}
+    <>
+      {cargando ? "Cargando..." :
+
+        articulos.length >= 1 ?
+          <Listado
+            articulos={articulos}
+            setArticulos={setArticulos}
+          />
+          : <h1>No hay artículos</h1>
+
+      }
+
+
+    </>
+  );
+};
