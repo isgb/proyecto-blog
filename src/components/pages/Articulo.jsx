2602433c5ab7e5a3783570a11c4f6578e2957137
isaias_pruebas <isgb_codigo@hotmail.com>
2024-01-27 14:20:26 -0600

    debug

--- src/components/pages/Articulo.jsx
+++ src/components/pages/Articulo.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Global } from "../../helpers/Global";
+import { Peticion } from "../../helpers/Peticion";
+import { Listado } from "./Listado";
+import { useParams } from "react-router-dom";
 
 export const Articulo = () => {
+  const [articulo, setArticulo] = useState({});
+  const [cargando, setCargando] = useState(true);
+  const params = useParams();
+
+  const conseguirArticulo = async () => {
+    const { datos, cargando } = await Peticion(
+      Global.url + "articulo/" + params.id,
+      "GET"
+    );
+
+    console.log("datos:", datos);
+    if (datos.status === "success") {
+      setArticulo(datos.Articulo);
+    }
+
+    setCargando(false);
+    console.log(articulo);
+  };
+
+  useEffect(() => {
+    conseguirArticulo();
+  }, []);
+
   return (
-    <div>Articulo</div>
-  )
-}
+    <>
+      <div className="jumbo">
+        {cargando ? (
+          "Cargando..."
+        ) : (
+          <>
+            <div className="mascara">
+              {articulo.imagen != "deafult.png" && (
+                <img src={Global.url + "imagen/" + articulo.imagen} />
+              )}
+              {articulo.imagen == "deafult.png" && (
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png" />
+              )}
+            </div>
+            <h1>{articulo.titulo}</h1>
+            <span>{articulo.fecha}</span>
+            <p>{articulo.contenido}</p>
+          </>
+        )}
+      </div>
+    </>
+  );
+};
--- src/components/pages/Listado.jsx
+++ src/components/pages/Listado.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Global } from '../../helpers/Global'
 import { Peticion } from '../../helpers/Peticion'
+import { Link } from 'react-router-dom'
 
 export const Listado = ({articulos, setArticulos}) => {
 
@@ -25,7 +26,7 @@ export const Listado = ({articulos, setArticulos}) => {
 
             </div>
             <div className="datos">
-              <h3 className="title">{articulo.titulo}</h3>
+              <h3 className="title"><Link to={"/articulo/"+articulo._id}>{articulo.titulo}</Link></h3>
               <p className="description">{articulo.contenido}</p>
 
               <button className="edit">Editar</button>
